26d2301fd499a6871e3b418fe0a304754789397a
jvroig <jvroig@gmail.com>
2025-08-29 10:13:14 +0800

    Update start.sh to accept optional model name

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 source venv/bin/activate
 venv/bin/pip install -r requirements.txt
-venv/bin/python qwen_api.py
+
+# Pass model name if provided as first argument
+if [ -n "$1" ]; then
+    venv/bin/python qwen_api.py --model "$1"
+else
+    venv/bin/python qwen_api.py
